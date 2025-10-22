9c7dea293ba445d457a3843b45467125337c4433
Luthetus <luthetus@gmail.com>
2025-10-22 11:09:06 -0400

    Update ITreeViewContainer.cs

--- Source/Lib/Common/TreeViews/Models/ITreeViewContainer.cs
+++ Source/Lib/Common/TreeViews/Models/ITreeViewContainer.cs
@@ -9,6 +9,9 @@ namespace Clair.Common.RazorLib.TreeViews.Models;
 /// </summary>
 public interface ITreeViewContainer : IDisposable
 {
+    /// <summary>Unique identifier</summary>
+    public Key<TreeViewContainer> Key { get; init; }
+    
     /// <summary>
     /// WARNING: modification of this list from a non-`ITreeViewContainer` is extremely unsafe.
     /// ...it is the responsibility of the container to ensure any modifications it performs
@@ -17,30 +20,22 @@ public interface ITreeViewContainer : IDisposable
     /// ... Making this an IReadOnlyList only serves to increase overhead when enumerating the NodeValue
     /// (and the enumeration of this is a hot path),
     /// just don't touch the list and there won't be any problems.
-    /// </summary>
-    public List<TreeViewNodeValue> NodeValueList { get; }
-    public Key<TreeViewContainer> Key { get; init; }
-    /// <summary>
-    /// The <see cref="ActiveNode"/> is the last or default entry in <see cref="SelectedNodeList"/>
-    /// </summary>
-    public TreeViewNoType? ActiveNode => SelectedNodeList.FirstOrDefault();
-    public IReadOnlyList<TreeViewNoType> SelectedNodeList { get; init; }
-    public Guid StateId { get; init; } = Guid.NewGuid();
-    /// <summary>
-    /// In your constructor:
-    /// `ActiveNodeElementId = $"ci_node-{Key.Guid}";`
-    /// </summary>
-    public string ActiveNodeElementId { get; }
-    /// <summary>Quite hacky</summary>
-    public string ElementIdOfComponentRenderingThis { get; set; }
-    /// <summary>
-    /// A TreeViewNoType's children are enumerated by the span of
+    ///
+    /// A TreeViewNodeValue's children are enumerated by the span of
     /// the container's (inclusive) ChildList[TreeViewNoType.ChildListOffset] to
     /// (exclusive) ChildList[TreeViewNoType.ChildListOffset + TreeViewNoType.ChildListLength].
     /// </summary>
-    public List<TreeViewNoType> ChildList { get; set; } = new();
+    public List<TreeViewNodeValue> NodeValueList { get; }
     
-    public List<> Asd;
+    public int ActiveNodeValueIndex { get; }
+    
+    /// <summary>
+    /// In your constructor:
+    /// `ActiveNodeElementId = $"ci_node-{Key.Guid}";`
+    ///
+    /// This is used for scrolling the ActiveNodeValueIndex into view.
+    /// </summary>
+    public string ActiveNodeElementId { get; }
     
     /// <summary>
     /// When the UI expands a nodeValue, then

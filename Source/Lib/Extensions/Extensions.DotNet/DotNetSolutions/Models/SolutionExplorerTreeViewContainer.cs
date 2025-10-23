2d199e6588dd624ebf73710491e7c2dd87a33562
Luthetus <luthetus@gmail.com>
2025-10-23 18:15:04 -0400

    Load directory

--- Source/Lib/Extensions/Extensions.DotNet/DotNetSolutions/Models/SolutionExplorerTreeViewContainer.cs
+++ Source/Lib/Extensions/Extensions.DotNet/DotNetSolutions/Models/SolutionExplorerTreeViewContainer.cs
@@ -211,6 +211,108 @@ public class SolutionExplorerTreeViewContainer : TreeViewContainer
                 NodeValueList[indexNodeValue] = nodeValue;
                 return Task.CompletedTask;
             }
+            case TreeViewNodeValueKind.b3: // dir
+            {
+                var directoryAbsolutePath = DirectoryTraitsList[nodeValue.TraitsIndex];
+
+                var directoryList = CommonService.FileSystemProvider.Directory.GetDirectories(directoryAbsolutePath.Value);
+                var fileList = CommonService.FileSystemProvider.Directory.GetFiles(directoryAbsolutePath.Value);
+
+                var tokenBuilder = new StringBuilder();
+                var formattedBuilder = new StringBuilder();
+
+                var childDirectoryNamespacePathList = directoryList
+                    .Where(x => !IdeFacts.IsHiddenFileByContainerFileExtension(CommonFacts.C_SHARP_PROJECT, x))
+                    .OrderBy(pathString => pathString)
+                    .Select(x =>
+                    {
+                        return new AbsolutePath(
+                            x, true, CommonService.FileSystemProvider, tokenBuilder, formattedBuilder, AbsolutePathNameKind.NameNoExtension);
+                    });
+
+                // The way I sort the unique to come before the default directories feels scuffed
+                // but it is an extremely minor detail relative to the task of rewriting the treeviews
+                foreach (var absolutePath in childDirectoryNamespacePathList)
+                {
+                    if (IdeFacts.IsUniqueFileByContainerFileExtension(CommonFacts.C_SHARP_PROJECT, absolutePath.Name))
+                    {
+                        DirectoryTraitsList.Add(absolutePath);
+                        NodeValueList.Add(new TreeViewNodeValue
+                        {
+                            ParentIndex = indexNodeValue,
+                            IndexAmongSiblings = 0,
+                            ChildListOffset = 0,
+                            ChildListLength = 0,
+                            TreeViewNodeValueKind = TreeViewNodeValueKind.b3, // dir
+                            TraitsIndex = DirectoryTraitsList.Count - 1,
+                            IsExpandable = true,
+                            IsExpanded = false,
+                        });
+                    }
+                }
+
+                // The way I sort the unique to come before the default directories feels scuffed
+                // but it is an extremely minor detail relative to the task of rewriting the treeviews
+                foreach (var absolutePath in childDirectoryNamespacePathList)
+                {
+                    if (!IdeFacts.IsUniqueFileByContainerFileExtension(CommonFacts.C_SHARP_PROJECT, absolutePath.Name))
+                    {
+                        DirectoryTraitsList.Add(absolutePath);
+                        NodeValueList.Add(new TreeViewNodeValue
+                        {
+                            ParentIndex = indexNodeValue,
+                            IndexAmongSiblings = 0,
+                            ChildListOffset = 0,
+                            ChildListLength = 0,
+                            TreeViewNodeValueKind = TreeViewNodeValueKind.b3, // dir
+                            TraitsIndex = DirectoryTraitsList.Count - 1,
+                            IsExpandable = true,
+                            IsExpanded = false,
+                        });
+                    }
+                }
+
+                var fileAbsolutePathList = fileList
+                    .Where(x => !x.EndsWith(CommonFacts.C_SHARP_PROJECT))
+                    .OrderBy(pathString => pathString)
+                    .Select(x =>
+                    {
+                        return new AbsolutePath(x, false, CommonService.FileSystemProvider, tokenBuilder, formattedBuilder, AbsolutePathNameKind.NameWithExtension);
+                    });
+
+                foreach (var absolutePath in fileAbsolutePathList)
+                {
+                    FileTraitsList.Add(absolutePath);
+                    NodeValueList.Add(new TreeViewNodeValue
+                    {
+                        ParentIndex = indexNodeValue,
+                        IndexAmongSiblings = 0,
+                        ChildListOffset = 0,
+                        ChildListLength = 0,
+                        TreeViewNodeValueKind = TreeViewNodeValueKind.b4, // file
+                        TraitsIndex = FileTraitsList.Count - 1,
+                        IsExpandable = false,
+                        IsExpanded = false,
+                    });
+                }
+
+                nodeValue.ChildListLength = NodeValueList.Count - nodeValue.ChildListOffset;
+
+                /*var cSharpProjectDependenciesTreeViewNode = new TreeViewCSharpProjectDependencies(
+                    new CSharpProjectDependencies(project.AbsolutePath),
+                    cSharpProjectTreeView.CommonService,
+                    true,
+                    false);*/
+
+                // file system list vs the filtered list has a negligible length difference vs the cost of enumerating filtered list to get length / internal reallocations of list.
+                /*var result = new List<TreeViewNodeValue>(capacity: directoryList.Length + fileList.Length)
+                {
+                    //cSharpProjectDependenciesTreeViewNode
+                };*/
+
+                NodeValueList[indexNodeValue] = nodeValue;
+                return Task.CompletedTask;
+            }
             default:
             {
                 break;

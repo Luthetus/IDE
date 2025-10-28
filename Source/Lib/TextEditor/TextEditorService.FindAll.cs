c7e432f9cf68987d05badde86e1ee243ac2d23b8
Luthetus <luthetus@gmail.com>
2025-10-28 11:06:06 -0400

    # INCORRECT Diagram

--- Source/Lib/TextEditor/TextEditorService.FindAll.cs
+++ Source/Lib/TextEditor/TextEditorService.FindAll.cs
@@ -565,8 +565,8 @@ public partial class TextEditorService
             	// b4 t1 o6 l2 i1 p0
                 //
                 // # INCORRECT Diagram
-                //   0, 1,    2,    3,    4,    5,    6,  7,  8
-                // [ R, S_f1, S_f1, S_f1, S_f1, S_f1, F1, P1, P2 ]
+                //      0,    1,       2,       3,       4,       5,       6,     7,     8,     9,     10
+                // [    R,    S_f1,    S_f1,    S_f1,    S_f1,    S_f1,    F1,    F2,    F3,    P1,    P2    ]
                 //
                 //
                 // ================================================
